c2defac3d68ff3ebe2b99166f27a1932574ca936
Jacinto Mba Cantero <jacmba@gmail.com>
2022-10-07 17:41:24 +0100

    Fixed interaction text display. Fixed timers UI

--- Assets/Scripts/Controller/UI/InteractionTextController.cs
+++ Assets/Scripts/Controller/UI/InteractionTextController.cs
@@ -7,10 +7,13 @@ public class InteractionTextController : MonoBehaviour {
   [SerializeField] private int durationSeconds = 5;
 
   private Text text;
+  private Coroutine lastCoroutine;
+  private bool crRunning;
 
   // Start is called before the first frame update
   void Start() {
     text = GetComponent<Text>();
+    crRunning = false;
 
     EventBus.OnMessageShow += OnMessageShow;
   }
@@ -20,12 +23,17 @@ public class InteractionTextController : MonoBehaviour {
   }
 
   private void OnMessageShow(string message) {
-    StartCoroutine(DisplayMessage(message));
+    if(lastCoroutine != null && crRunning) {
+      StopCoroutine(lastCoroutine);
+    }
+    lastCoroutine = StartCoroutine(DisplayMessage(message));
   }
 
   private IEnumerator DisplayMessage(string message) {
+    crRunning = true;
     text.text = message;
     yield return new WaitForSeconds(durationSeconds);
     text.text = "";
+    crRunning = false;
   }
 }
