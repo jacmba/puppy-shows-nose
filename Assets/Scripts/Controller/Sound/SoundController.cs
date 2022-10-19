238b7ad1b9ebf78131fc565e7d0bf599e971050f
Jacinto Mba Cantero <jacmba@gmail.com>
2022-10-19 10:37:13 +0100

    Mascleta sound

--- Assets/Scripts/Controller/Sound/SoundController.cs
+++ Assets/Scripts/Controller/Sound/SoundController.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class SoundController : MonoBehaviour {
   [SerializeField] AudioClip itemSound;
+  [SerializeField] AudioClip mascletaSound;
 
   private AudioSource source;
 
   void Awake() {
     source = GetComponent<AudioSource>();
     EventBus.OnShoppingItemClear += OnItemPick;
+    EventBus.OnToiletEnter += OnMascleta;
+    EventBus.OnPuppyShown += OnMascleta;
   }
 
   void OnDestroy() {
     EventBus.OnItemPick -= OnItemPick;
+    EventBus.OnToiletEnter -= OnMascleta;
+    EventBus.OnPuppyShown -= OnMascleta;
   }
 
   private void OnItemPick(ShoppingItemType item) {
     source.clip = itemSound;
+    source.Play();
+  }
+
+  private void OnMascleta() {
+    source.clip = mascletaSound;
     source.Play();
   }
 }
